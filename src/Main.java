ffb21f7a63fb969e50b92bb5472c48b1435dc1e9
Yenni Delgado <yennivanessadelgado@gmail.com>
2025-10-03 16:08:31 -0500

    Se añadió la clase que representa un nodo del árbol binario

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+/**
+ * Clase que representa un nodo del árbol binario.
+ * Cada nodo contiene un dato de tipo entero y referencias a su hijo izquierdo y derecho.
+ */
+class Nodo {
+    int dato;              // Valor que almacena el nodo
+    Nodo izquierda, derecha; // Referencias a los nodos hijos
+
+    /**
+     * Constructor del nodo
+     * @param dato Valor entero que contendrá este nodo
+     */
+    public Nodo(int dato) {
+        this.dato = dato;
+        izquierda = derecha = null; // Inicialmente sin hijos
     }
 }
